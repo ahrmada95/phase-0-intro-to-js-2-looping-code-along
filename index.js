b33e7c0d0c1ddcc20d772fc54d885e8bcc2b28dc
Ahnaf Ahmad <ahrmada95@gmail.com>
2022-06-16 19:55:49 -0400

    Finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(names,eventName) {
+    const tyMessages = [];
+    for (let a = 0; a < names.length; a++) {
+        tyMessages.push(`Thank you, ${names[a]}, for the wonderful ${eventName} gift!`);
+    }
+    return tyMessages;
+}
+
+function countDown(number) {
+    // while (number >= 0) {
+    //     console.log(number);
+    //     number--;
+    // }
+
+    for (let a = number; a >= 0; a--){
+        console.log(a);
+    }
+}
